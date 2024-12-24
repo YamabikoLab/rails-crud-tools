6ee0bd8caf1c4ba3b97ada6b0e58805a3fb3791e
yhijikata <yhijikata@systemlancer.com>
2024-12-25 06:14:24 +0900

    マルチスレッド改善

--- lib/rails/crud/crud_notifications.rb
+++ lib/rails/crud/crud_notifications.rb
@@ -1,6 +1,3 @@
-require_relative "crud_config"
-require_relative 'crud_logger'
-
 module Rails
   module Crud
     def self.setup_notifications
@@ -15,12 +12,17 @@ module Rails
                         else "Unknown"
                         end
 
-            table_name = data[:sql].match(/(?:INSERT INTO|UPDATE|DELETE FROM|FROM)\s+`?(\w+)`?/i)[1]
+            match_data = data[:sql].match(/(?:INSERT INTO|UPDATE|DELETE FROM|FROM)\s+`?(\w+)`?/i)
+            if match_data
+              table_name = match_data[1]
 
-            # CrudOperations インスタンスを使用して操作を追加
-            CrudOperations.instance.add_operation(table_name, operation)
+              # CrudOperations インスタンスを使用して操作を追加
+              CrudOperations.instance.add_operation(table_name, operation)
 
-            CrudLogger.logger.info "#{data[:name]} - #{data[:sql]}"
+              CrudLogger.logger.info "#{data[:name]} - #{data[:sql]}"
+            else
+              CrudLogger.logger.warn "Table name not found in SQL: #{data[:sql]}"
+            end
           end
         end
       end
--- lib/rails/crud/crud_operations_logger.rb
+++ lib/rails/crud/crud_operations_logger.rb
@@ -7,29 +7,36 @@ module Rails
         if CrudConfig.instance.enabled
           CrudOperations.instance.table_operations = {}
           method = request.request_method
-          CrudLogger.logger.info "***** Method: #{method}, Controller: #{controller_path}, Action: #{action_name}, Key: #{controller_path}##{action_name} *****"
+          CrudLogger.logger.info "******************** Method: #{method}, Controller: #{controller_path}, Action: #{action_name}, Key: #{controller_path}##{action_name} ********************"
         end
 
         yield
+
+        CrudOperations.instance.log_operations
+
+        table_operations_copy = CrudOperations.instance.table_operations.dup
+        method_copy = method.dup
+        controller_path_copy = controller_path.dup
+        action_name_copy = action_name.dup
 
         if CrudConfig.instance.enabled
           Thread.new do
             sheet = CrudData.instance.workbook[0]
 
-            CrudOperations.instance.table_operations.each_key do |table_name|
-              row = CrudData.instance.crud_rows[method]["#{controller_path}##{action_name}"]
+            table_operations_copy.each_key do |table_name|
+              row = CrudData.instance.crud_rows[method_copy]["#{controller_path_copy}##{action_name_copy}"]
               col = CrudData.instance.crud_cols[table_name]
 
               # colまたはrowが存在しない場合にログ出力してスキップ
               unless row && col
-                Rails.logger.warn "Row or Column not found for table: #{table_name}, method: #{method}, controller: #{controller_path}, action: #{action_name}, row: #{row}, col: #{col}"
+                Rails.logger.warn "Row or Column not found for table: #{table_name}, method: #{method_copy}, controller: #{controller_path_copy}, action: #{action_name_copy}, row: #{row}, col: #{col}"
                 next
               end
-              # 既存の値を取得
+
               existing_value = sheet[row][col].value || ""
 
               # 新しい値と既存の値を結合し、重複を排除
-              new_value = CrudOperations.instance.table_operations[table_name].join
+              new_value = table_operations_copy[table_name].join
               merged_value = (existing_value.chars + new_value.chars).uniq
 
               # CRUDの順序に並び替え
@@ -42,8 +49,6 @@ module Rails
 
             # Excelファイルを書き込む
             CrudData.instance.workbook.write(CrudConfig.instance.crud_file_path)
-
-            CrudOperations.instance.log_operations
           end
         end
       end
