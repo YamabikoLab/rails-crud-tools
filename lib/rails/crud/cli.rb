3c95440e5e77e14e58fe504a14f3358bd374b417
yhijikata <yhijikata@systemlancer.com>
2024-12-29 19:37:39 +0900

    コマンドを実装

--- lib/rails/crud/cli.rb
+++ lib/rails/crud/cli.rb
@@ -1,10 +1,4 @@
-# frozen_string_literal: true
-
-require "rubyXL/convenience_methods/cell"
-require "rubyXL/convenience_methods/color"
-require "rubyXL/convenience_methods/font"
-require "rubyXL/convenience_methods/workbook"
-require "rubyXL/convenience_methods/worksheet"
+require 'rubyXL/convenience_methods'
 
 module RailsCrud
   class CLI
