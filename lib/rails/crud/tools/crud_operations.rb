973fef67d19b53af92c93b864b7ccaf07a7376d4
yhijikata <yhijikata@systemlancer.com>
2025-01-01 06:12:50 +0900

    マルチスレッド対応

--- lib/rails/crud/tools/crud_operations.rb
+++ lib/rails/crud/tools/crud_operations.rb
@@ -1,4 +1,5 @@
 require_relative 'crud_logger'
+require_relative 'constants'
 
 module Rails
   module Crud
@@ -13,8 +14,24 @@ module Rails
         end
 
         def add_operation(table_name, operation)
-          @table_operations[table_name] ||= []
-          @table_operations[table_name] << operation unless @table_operations[table_name].include?(operation)
+          request = Thread.current[:request]
+          if request
+            method = request.request_method
+            controller = request.params['controller']
+            action = request.params['action']
+            key = "#{controller}##{action}"
+          elsif Thread.current[:sidekiq_job_class]
+            key = Thread.current[:sidekiq_job_class]
+            method = Constants::DEFAULT_METHOD
+          else
+            CrudLogger.logger.warn "Unknown method and key detected"
+            return
+          end
+
+          @table_operations[method] ||= {}
+          @table_operations[method][key] ||= {}
+          @table_operations[method][key][table_name] ||= []
+          @table_operations[method][key][table_name] << operation unless @table_operations[method][key][table_name].include?(operation)
         end
 
         def log_operations(key, method = nil)
@@ -24,7 +41,7 @@ module Rails
             CrudLogger.logger.info "\nSummary: Key: #{key}"
           end
 
-          @table_operations.each do |table_name, operations|
+          @table_operations[method][key].each do |table_name, operations|
             CrudLogger.logger.info "#{table_name} - #{operations.join(', ')}"
           end
         end
