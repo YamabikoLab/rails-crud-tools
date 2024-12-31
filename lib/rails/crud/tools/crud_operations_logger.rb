f8e9344cfc8d19565459673ca687c199e4fa714d
yhijikata <yhijikata@systemlancer.com>
2025-01-01 08:34:02 +0900

    スレッド削除

--- lib/rails/crud/tools/crud_operations_logger.rb
+++ lib/rails/crud/tools/crud_operations_logger.rb
@@ -71,45 +71,43 @@ module Rails
           method_copy = method.nil? ? Constants::DEFAULT_METHOD : method.dup
           key_copy = key.dup
 
-          Thread.new do
-            table_operations_copy.each_key do |table_name|
-              row = CrudData.instance.crud_rows[method_copy][key_copy]
-              col = CrudData.instance.crud_cols[table_name]
+          table_operations_copy.each_key do |table_name|
+            row = CrudData.instance.crud_rows[method_copy][key_copy]
+            col = CrudData.instance.crud_cols[table_name]
 
-              # colまたはrowが存在しない場合にログ出力してスキップ
-              unless row && col
-                CrudLogger.logger.warn "Row or Column not found for table: #{table_name}, method: #{method_copy}, key: #{key_copy}, row: #{row}, col: #{col}"
-                next
-              end
-
-              # sheet[row][col]がnilの場合に警告文を出力し、空文字列として処理を進める
-              cell = sheet[row][col]
-              if cell.nil?
-                cell = sheet.add_cell(row, col, "")
-                CrudLogger.logger.warn "Cell not found at row: #{row}, col: #{col} for table: #{table_name}, method: #{method_copy}, key: #{key_copy}. Adding new cell."
-                existing_value = ""
-              else
-                existing_value = cell.value || ""
-              end
-
-              # 新しい値と既存の値を結合し、重複を排除
-              new_value = table_operations_copy[table_name].join
-              merged_value = (existing_value.chars + new_value.chars).uniq
-
-              # CRUDの順序に並び替え
-              crud_order = %w[C R U D]
-              sorted_value = merged_value.sort_by { |char| crud_order.index(char) }.join
-
-              cell.change_contents(sorted_value)
+            # colまたはrowが存在しない場合にログ出力してスキップ
+            unless row && col
+              CrudLogger.logger.warn "Row or Column not found for table: #{table_name}, method: #{method_copy}, key: #{key_copy}, row: #{row}, col: #{col}"
+              next
             end
 
-            # Excelファイルを書き込む
-            CrudData.instance.workbook.write(CrudConfig.instance.crud_file_path)
-            timestamp = File.mtime(CrudConfig.instance.crud_file_path)
-            CrudLogger.logger.debug "Updated timestamp: #{timestamp}"
-            # タイムスタンプを更新する
-            CrudData.instance.instance_variable_set(:@last_loaded_time, timestamp)
+            # sheet[row][col]がnilの場合に警告文を出力し、空文字列として処理を進める
+            cell = sheet[row][col]
+            if cell.nil?
+              cell = sheet.add_cell(row, col, "")
+              CrudLogger.logger.warn "Cell not found at row: #{row}, col: #{col} for table: #{table_name}, method: #{method_copy}, key: #{key_copy}. Adding new cell."
+              existing_value = ""
+            else
+              existing_value = cell.value || ""
+            end
+
+            # 新しい値と既存の値を結合し、重複を排除
+            new_value = table_operations_copy[table_name].join
+            merged_value = (existing_value.chars + new_value.chars).uniq
+
+            # CRUDの順序に並び替え
+            crud_order = %w[C R U D]
+            sorted_value = merged_value.sort_by { |char| crud_order.index(char) }.join
+
+            cell.change_contents(sorted_value)
           end
+
+          # Excelファイルを書き込む
+          CrudData.instance.workbook.write(CrudConfig.instance.crud_file_path)
+          timestamp = File.mtime(CrudConfig.instance.crud_file_path)
+          CrudLogger.logger.debug "Updated timestamp: #{timestamp}"
+          # タイムスタンプを更新する
+          CrudData.instance.instance_variable_set(:@last_loaded_time, timestamp)
         end
       end
     end
