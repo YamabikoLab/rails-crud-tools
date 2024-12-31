5dd96bcecd3be1c084d7edd8dcb96815fe36de9d
yhijikata <yhijikata@systemlancer.com>
2024-12-31 09:06:47 +0900

    rails-crud-toolsに変更

--- lib/rails/crud/tools.rb
+++ lib/rails/crud/tools.rb
@@ -4,11 +4,12 @@ require "rubyXL/convenience_methods/color"
 require "rubyXL/convenience_methods/font"
 require "rubyXL/convenience_methods/workbook"
 require "rubyXL/convenience_methods/worksheet"
-require_relative 'tools/crud_operations'
-require_relative "tools/config"
+require_relative "tools/crud_operations"
+require_relative "tools/crud_config"
 require_relative "tools/crud_notifications"
 require_relative "tools/railtie"
 require_relative "tools/crud_data"
+
 module Rails
   module Crud
     module Tools
