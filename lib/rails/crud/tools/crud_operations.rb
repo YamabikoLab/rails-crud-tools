90012a3e0eaf79a376fc26b4747fdc904c6877cc
yhijikata <yhijikata@systemlancer.com>
2025-01-01 06:55:19 +0900

    マルチスレッド対応

--- lib/rails/crud/tools/crud_operations.rb
+++ lib/rails/crud/tools/crud_operations.rb
@@ -10,9 +10,13 @@ module Rails
         attr_accessor :table_operations, :logs
 
         def initialize
-          CrudLogger.logger.debug "Initializing CrudOperations"
-          @table_operations = Hash.new { |hash, key| hash[key] = Hash.new { |h, k| h[k] = Hash.new { |hh, kk| hh[kk] = [] } } }
-          CrudLogger.logger.debug "Initialized @table_operations with default values"
+          @table_operations = Hash.new do |hash, method|
+            hash[method] = Hash.new do |h, key|
+              h[key] = Hash.new do |hh, table|
+                hh[table] = []
+              end
+            end
+          end
         end
 
         def add_operation(table_name, operation)
@@ -38,16 +42,6 @@ module Rails
             CrudLogger.logger.info "\nSummary: Method: #{method}, Key: #{key}"
           else
             CrudLogger.logger.info "\nSummary: Key: #{key}"
-          end
-
-          if @table_operations[method].nil?
-            CrudLogger.logger.error "Method #{method} does not exist in @table_operations"
-            return
-          end
-
-          if @table_operations[method][key].nil?
-            CrudLogger.logger.error "Key #{key} does not exist for method #{method} in @table_operations"
-            return
           end
 
           @table_operations[method][key].each do |table_name, operations|
