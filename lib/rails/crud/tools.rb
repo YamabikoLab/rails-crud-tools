645b067ee1a070c4585eef2c62f64f3f62b6c2d8
yhijikata <yhijikata@systemlancer.com>
2025-01-04 09:06:01 +0900

    bugfix

--- lib/rails/crud/tools.rb
+++ lib/rails/crud/tools.rb
@@ -1,4 +1,13 @@
+require "rubyXL"
+require "rubyXL/convenience_methods/cell"
+require "rubyXL/convenience_methods/color"
+require "rubyXL/convenience_methods/font"
+require "rubyXL/convenience_methods/workbook"
+require "rubyXL/convenience_methods/worksheet"
+require_relative "tools/crud_operations"
+require_relative "tools/crud_config"
 require_relative "tools/crud_notifications"
+require_relative "tools/railtie"
 require_relative "tools/crud_data"
 
 module Rails
