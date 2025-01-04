02024a725f2d7ab1327a1d51f1c082a2f8920416
yhijikata <yhijikata@systemlancer.com>
2025-01-04 09:23:35 +0900

    リファクタリング

--- lib/rails/crud/tools.rb
+++ lib/rails/crud/tools.rb
@@ -14,8 +14,15 @@ module Rails
   module Crud
     module Tools
       def self.setup()
-        return unless File.exist?(".crudconfig")
-        return unless File.exist?(CrudConfig.instance.crud_file_path)
+        unless File.exist?(".crudconfig")
+          puts "The .crudconfig file is required. Please run `bundle exec crud init`."
+          return
+        end
+
+        unless File.exist?(CrudConfig.instance.crud_file_path)
+          puts "The CRUD file is required. Please run `bundle exec crud gen_crud`."
+          return
+        end
 
         CrudData.instance.load_crud_data
         setup_notifications
