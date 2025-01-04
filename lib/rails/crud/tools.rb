c2c74b37feda1fcb44c50db64f769a7a1fa7f7d9
yhijikata <yhijikata@systemlancer.com>
2025-01-04 09:00:58 +0900

    bugfix

--- lib/rails/crud/tools.rb
+++ lib/rails/crud/tools.rb
@@ -1,24 +1,17 @@
-require "rubyXL"
-require "rubyXL/convenience_methods/cell"
-require "rubyXL/convenience_methods/color"
-require "rubyXL/convenience_methods/font"
-require "rubyXL/convenience_methods/workbook"
-require "rubyXL/convenience_methods/worksheet"
-require_relative "tools/crud_operations"
-require_relative "tools/crud_config"
 require_relative "tools/crud_notifications"
-require_relative "tools/railtie"
 require_relative "tools/crud_data"
 
 module Rails
   module Crud
     module Tools
-      def self.load_crud_data
+      def self.setup()
+        return unless File.exist?(".crudconfig")
+
         CrudData.instance.load_crud_data
+        setup_notifications
       end
 
-      self.load_crud_data
-      setup_notifications
+      setup
     end
   end
 end
