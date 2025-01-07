42cbb6a2142ede115de233c1d33d74eb3b61e95d
yhijikata <yhijikata@systemlancer.com>
2025-01-08 07:08:01 +0900

    refactoring

--- lib/rails/crud/tools/crud_notifications.rb
+++ lib/rails/crud/tools/crud_notifications.rb
@@ -30,85 +30,13 @@ module Rails
 
         case data[:sql]
         when /\bINSERT INTO\b.*\bSELECT\b/i
-          # INSERT INTO ... SELECT の特別な処理
-          insert_table = data[:sql].match(/INSERT INTO\s+`?(\w+)`?/i)[1]
-          select_tables = data[:sql].scan(/SELECT .* FROM\s+`?(\w+)`?(?:\s*,\s*`?(\w+)`?)*|JOIN\s+`?(\w+)`?/i).flatten.compact
-
-          key, method = determine_key_and_method
-          if key.nil? || method.nil?
-            CrudLogger.logger.warn "Request not found. #{data[:sql]}"
-            return
-          end
-
-          CrudOperations.instance.add_operation(method, key, insert_table, "C")
-          select_tables.each do |select_table|
-            CrudOperations.instance.add_operation(method, key, select_table, "R")
-          end
+          handle_insert_select(data)
         when /\bUPDATE\b.*\bSET\b.*\bSELECT\b/i
-          # UPDATE ... SET ... SELECT の特別な処理
-          update_table = data[:sql].match(/UPDATE\s+`?(\w+)`?/i)[1]
-          select_tables = data[:sql].scan(/SELECT .* FROM\s+`?(\w+)`?(?:\s*,\s*`?(\w+)`?)*|JOIN\s+`?(\w+)`?/i).flatten.compact
-
-          key, method = determine_key_and_method
-          if key.nil? || method.nil?
-            CrudLogger.logger.warn "Request not found. #{data[:sql]}"
-            return
-          end
-
-          CrudOperations.instance.add_operation(method, key, update_table, "U")
-          select_tables.each do |select_table|
-            CrudOperations.instance.add_operation(method, key, select_table, "R")
-          end
+          handle_update_select(data)
         when /\bDELETE\b.*\bEXISTS\b.*\bSELECT\b/i
-          # DELETE ... WHERE EXISTS ... SELECT の特別な処理
-          delete_table = data[:sql].match(/DELETE FROM\s+`?(\w+)`?/i)[1]
-          select_tables = data[:sql].scan(/SELECT .* FROM\s+`?(\w+)`?(?:\s*,\s*`?(\w+)`?)*|JOIN\s+`?(\w+)`?/i).flatten.compact
-
-          key, method = determine_key_and_method
-          if key.nil? || method.nil?
-            CrudLogger.logger.warn "Request not found. #{data[:sql]}"
-            return
-          end
-
-          CrudOperations.instance.add_operation(method, key, delete_table, "D")
-          select_tables.each do |select_table|
-            CrudOperations.instance.add_operation(method, key, select_table, "R")
-          end
+          handle_delete_select(data)
         else
-          operation = if (match = data[:sql].match(/\A\s*(INSERT|UPDATE|DELETE|SELECT)/i))
-                        case match[1].upcase
-                        when "INSERT" then "C"
-                        when "SELECT" then "R"
-                        when "UPDATE" then "U"
-                        when "DELETE" then "D"
-                        else OPERATION_UNKNOWN
-                        end
-                      else
-                        OPERATION_UNKNOWN
-                      end
-
-          if operation == OPERATION_UNKNOWN
-            warn "Warning: Unknown SQL operation. SQL: #{data[:sql]}"
-            return
-          end
-
-          table_names = data[:sql].scan(/(?:INSERT INTO|UPDATE|DELETE FROM|FROM|JOIN)\s+`?(\w+)`?(?:\s*,\s*`?(\w+)`?)*/i).flatten.compact
-          if table_names.empty?
-            # テーブル名が見つからない場合は警告を出力
-            CrudLogger.logger.warn "Table name not found in SQL: #{data[:sql]}"
-            return
-          end
-
-          key, method = determine_key_and_method
-          if key.nil? || method.nil?
-            CrudLogger.logger.warn "Request not found. #{data[:sql]}"
-            return
-          end
-
-          # テーブル名を取得して CRUD 操作に追加
-          table_names.each do |table_name|
-            CrudOperations.instance.add_operation(method, key, table_name, operation)
-          end
+          handle_general_sql(data)
         end
 
         return unless CrudConfig.instance.sql_logging_enabled
@@ -117,6 +45,93 @@ module Rails
         CrudLogger.logger.info (data[:sql]).to_s
       end
 
+      def self.handle_insert_select(data)
+        # INSERT INTO ... SELECT の特別な処理
+        insert_table = data[:sql].match(/INSERT INTO\s+`?(\w+)`?/i)[1]
+        select_tables = data[:sql].scan(/SELECT .* FROM\s+`?(\w+)`?(?:\s*,\s*`?(\w+)`?)*|JOIN\s+`?(\w+)`?/i).flatten.compact
+
+        key, method = determine_key_and_method
+        if key.nil? || method.nil?
+          CrudLogger.logger.warn "Request not found. #{data[:sql]}"
+          return
+        end
+
+        CrudOperations.instance.add_operation(method, key, insert_table, "C")
+        select_tables.each do |select_table|
+          CrudOperations.instance.add_operation(method, key, select_table, "R")
+        end
+      end
+
+      def self.handle_update_select(data)
+        # UPDATE ... SET ... SELECT の特別な処理
+        update_table = data[:sql].match(/UPDATE\s+`?(\w+)`?/i)[1]
+        select_tables = data[:sql].scan(/SELECT .* FROM\s+`?(\w+)`?(?:\s*,\s*`?(\w+)`?)*|JOIN\s+`?(\w+)`?/i).flatten.compact
+
+        key, method = determine_key_and_method
+        if key.nil? || method.nil?
+          CrudLogger.logger.warn "Request not found. #{data[:sql]}"
+          return
+        end
+
+        CrudOperations.instance.add_operation(method, key, update_table, "U")
+        select_tables.each do |select_table|
+          CrudOperations.instance.add_operation(method, key, select_table, "R")
+        end
+      end
+
+      def self.handle_delete_select(data)
+        # DELETE ... WHERE EXISTS ... SELECT の特別な処理
+        delete_table = data[:sql].match(/DELETE FROM\s+`?(\w+)`?/i)[1]
+        select_tables = data[:sql].scan(/SELECT .* FROM\s+`?(\w+)`?(?:\s*,\s*`?(\w+)`?)*|JOIN\s+`?(\w+)`?/i).flatten.compact
+
+        key, method = determine_key_and_method
+        if key.nil? || method.nil?
+          CrudLogger.logger.warn "Request not found. #{data[:sql]}"
+          return
+        end
+
+        CrudOperations.instance.add_operation(method, key, delete_table, "D")
+        select_tables.each do |select_table|
+          CrudOperations.instance.add_operation(method, key, select_table, "R")
+        end
+      end
+
+      def self.handle_general_sql(data)
+        operation = if (match = data[:sql].match(/\A\s*(INSERT|UPDATE|DELETE|SELECT)/i))
+                      case match[1].upcase
+                      when "INSERT" then "C"
+                      when "SELECT" then "R"
+                      when "UPDATE" then "U"
+                      when "DELETE" then "D"
+                      else OPERATION_UNKNOWN
+                      end
+                    else
+                      OPERATION_UNKNOWN
+                    end
+
+        if operation == OPERATION_UNKNOWN
+          warn "Warning: Unknown SQL operation. SQL: #{data[:sql]}"
+          return
+        end
+
+        table_names = data[:sql].scan(/(?:INSERT INTO|UPDATE|DELETE FROM|FROM|JOIN)\s+`?(\w+)`?(?:\s*,\s*`?(\w+)`?)*/i).flatten.compact
+        if table_names.empty?
+          # テーブル名が見つからない場合は警告を出力
+          CrudLogger.logger.warn "Table name not found in SQL: #{data[:sql]}"
+          return
+        end
+
+        key, method = determine_key_and_method
+        if key.nil? || method.nil?
+          CrudLogger.logger.warn "Request not found. #{data[:sql]}"
+          return
+        end
+
+        # テーブル名を取得して CRUD 操作に追加
+        table_names.each do |table_name|
+          CrudOperations.instance.add_operation(method, key, table_name, operation)
+        end
+      end
       # キーとメソッドを決定する
       def self.determine_key_and_method
         request = Thread.current[:crud_request]
