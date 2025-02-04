02fed1b358d584cd96a1515e75332da0b6a57e47
yhijikata <yhijikata@systemlancer.com>
2025-02-05 07:16:51 +0900

    Refactor last modified tracking in CrudOperationsLogger
    - Move last modified by setting outside of file write block
    - Use CrudConfig's file path for setting last modified metadata
    - Simplify file operation logic in write method

--- lib/rails/crud/tools/crud_operations_logger.rb
+++ lib/rails/crud/tools/crud_operations_logger.rb
@@ -160,7 +160,6 @@ module Rails
             begin
               # Excelファイルを書き込む
               CrudData.instance.workbook.write(crud_file)
-              set_last_modified_by(crud_file, CrudData.instance.process_id)
               timestamp = File.mtime(crud_file)
               # タイムスタンプを更新する
               CrudData.instance.last_loaded_time = timestamp
@@ -169,6 +168,9 @@ module Rails
               crud_file.flock(File::LOCK_UN)
             end
           end
+
+          # 最終更新者を設定
+          set_last_modified_by(CrudConfig.instance.config.crud_file_path, CrudData.instance.process_id)
         end
       end
     end
